2f2a19fc97e0ca8030ac17620134e23c3f1e13aa
Karol Bialas <>
2024-04-30 13:23:21 +0200

    Templates for creating service account with cluster-admin permissions

--- scripts/GenerateAccount.sh
+++ scripts/GenerateAccount.sh
@@ -20,6 +20,9 @@ replace_namespace_placeholder_in_file() {
     # Replace placeholder NAMESPACE with the given value
     placeholder="NAMESPACE"
     templateContent="${templateContent//\%$placeholder\%/$namespace}"
+    # Replace placeholder ENV with the given value
+    placeholder="ENV"
+    templateContent="${templateContent//\%$placeholder\%/$envSuffix}"
 
     # Create target directory if it doesn't exist
     mkdir -p "$outputDirectory"
@@ -109,6 +112,7 @@ templateContent="${templateContent//\%NAMESPACE\%/$namespace}"
 templateContent="${templateContent//\%CA_CERT\%/$caCrtBase64}"
 templateContent="${templateContent//\%TOKEN\%/$tokenDecoded}"
 templateContent="${templateContent//\%KUBE_SERVER_URL\%/$serverUrl}"
+templateContent="${templateContent//\%ENV\%/$envSuffix}"
 
 mkdir -p "$outputDirectory/config"
 
